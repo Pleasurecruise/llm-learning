83bdde2658d5d1dd3c811ef8e6fb51cdd7023940
Pleasurecruise <3196812536@qq.com>
2025-02-05 17:40:26 +0800

    fix: .gitignore problem

--- deepseek-r1/.ipynb_checkpoints/download-checkpoint.py
+++
@@ -1,3 +0,0 @@
-from modelscope import snapshot_download
-
-model_dir = snapshot_download('deepseek-ai/DeepSeek-R1-Distill-Qwen-7B', cache_dir='/root/autodl-tmp/llm-learning/model', revision='master')
